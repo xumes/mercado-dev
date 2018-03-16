084a6c5054c8ce179c4dc6fda63f427e17f7ed0b
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-03-15 23:08:58 -0300

    code refactored to better clarity

---
+++ src/Home.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+
+import AnuncioHome from './AnuncioHome'
+import Categorias from './Categorias'
+import base from './base'
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+          categorias: [],
+          anuncios: []
+        }
+    
+        base.bindToState('categorias', {
+          context: this,
+          state: 'categorias'
+        })
+    
+        base.bindToState('anuncios', {
+          context: this,
+          state: 'anuncios',
+          queries: {
+            limitToLast: 3
+          }
+        })
+    
+      }
+
+    render() {
+        return (
+            <div className="container">
+                <h3>Últimos anúncios</h3>
+                <div className="row">
+                    {this.state.anuncios.map((anuncio, index) => {
+                        return <AnuncioHome key={index} anuncio={anuncio} />
+                    })}
+
+                </div>
+                <h3>Categorias</h3>
+                {/*JSON.stringify(this.state.categorias)*/}
+                <div className="row">
+                    {this.state.categorias.map((cat, index) => {
+                        return [
+                            <Categorias categoria={cat} key={index} />,
+                            ++index % 4 === 0 && <div key={'c' + index} className="w-100"></div>
+                        ]
+                    })}
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home
